2b70907338844191d219ec14e2caeb7d7f794d75
R. Akbar <iamrakbar@gmail.com>
2024-04-12 22:03:26 +0700

    [WIP]

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 // `app/about/page.tsx` is the UI for the `/about` URL
 export default function Page() {
     return (
-        <div className="flex items-center justify-center p-24 space-y-8">
+        <div className="flex flex-col items-center justify-center p-24 space-y-8">
             <h1>Hello, This is About Page!</h1>
             <Link href="/">Go to Index</Link>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <main className="flex items-center justify-center p-24 space-y-8">
+        <main className="flex flex-col items-center justify-center p-24 space-y-8">
             <h1>Hello, This is Index Page!</h1>
             <Link href="/about">Go to About</Link>
         </main>
